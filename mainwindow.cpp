4d25f95010ad54910f6bece6bb391c0ee537ffae
Nathan Campos <eeepc904@gmail.com>
2013-05-20 10:56:47 -0300

    Everything is working

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+using namespace std;
+
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
 
@@ -11,7 +13,17 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
     tolerance = ui->cmbTolerance;
     result = ui->lblResult;
 
-    result->setText(QString(calc.values[0].name) + " " + calc.values[1].name);
+    // Load defaults.
+    bands.push_back("black");
+    bands.push_back("black");
+    bands.push_back("black");
+    bands.push_back("gold");
+
+    vector<const char*> tmp_bands;
+    for (size_t i = 0; i < bands.size(); ++i) {
+        tmp_bands.push_back(bands[i].toStdString().c_str());
+    }
+    set_result(resistance_calc.pretty_print(resistance_calc.calculate(bands), bands[bands.size() - 1].toStdString().c_str()));
 }
 
 MainWindow::~MainWindow() {
@@ -19,12 +31,47 @@ MainWindow::~MainWindow() {
 }
 
 void MainWindow::set_result(const QString &result_str) {
-    QString formatted_result = "<html><head/><body><p><span style='font-size:16pt; font-weight:600;'>" +
+    QString formatted_result = "<span style='font-size:16pt; font-weight:600;'>" +
             result_str +
-            "</span></p></body></html>";
+            "</span>";
     result->setText(formatted_result);
 }
 
-void MainWindow::on_cmbBand1_currentTextChanged(const QString &arg1) {
-    set_result(arg1);
+void MainWindow::set_band(int index, const QString &color) {
+    /*// Create a copy.
+    vector<const char*> tmp_bands;
+    for (size_t i = 0; i < bands.size(); ++i) {
+        if (i != index) {
+            tmp_bands.push_back(bands[i]);
+        } else {
+            tmp_bands.push_back(color.toLower().toStdString().c_str());
+        }
+    }
+
+    // Repopulate.
+    bands.clear();
+    for (size_t i = 0; i < tmp_bands.size(); ++i) {
+        bands.push_back(tmp_bands[i]);
+    }*/
+    bands[index] = color.toLower();
+
+    cout << bands[index].toStdString() << endl;
+    set_result(resistance_calc.pretty_print(resistance_calc.calculate(bands), bands[bands.size() - 1].toStdString().c_str()));
+}
+
+void MainWindow::on_cmbBand1_currentTextChanged(const QString &value) {
+    set_band(0, value);
+}
+
+void MainWindow::on_cmbBand2_currentTextChanged(const QString &value) {
+    cout << value.toLower().toStdString().c_str() << endl;
+    set_band(1, value);
+}
+
+void MainWindow::on_cmbMultiplier_currentTextChanged(const QString &value) {
+    set_band(2, value);
+}
+
+void MainWindow::on_cmbTolerance_currentTextChanged(const QString &value) {
+    set_band(3, value);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -6,6 +6,10 @@
 #include <QLabel>
 #include <QString>
 
+#include <vector>
+#include <iostream>
+#include <cstring>
+
 #include "resistance_calc.h"
 
 namespace Ui {
@@ -24,15 +28,21 @@ class MainWindow : public QMainWindow {
         QComboBox *tolerance;
         QLabel *result;
 
-        ResistanceCalc calc;
+        std::vector<QString> bands;
+        ResistanceCalc resistance_calc;
 
     public:
         explicit MainWindow(QWidget *parent = 0);
         ~MainWindow();
 
         void set_result(const QString &result_str);
-private slots:
-        void on_cmbBand1_currentTextChanged(const QString &arg1);
+        void set_band(int index, const QString &color);
+
+    private slots:
+        void on_cmbBand1_currentTextChanged(const QString &value);
+        void on_cmbBand2_currentTextChanged(const QString &value);
+        void on_cmbMultiplier_currentTextChanged(const QString &value);
+        void on_cmbTolerance_currentTextChanged(const QString &value);
 };
 
 #endif // MAINWINDOW_H
